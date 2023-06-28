819d36334e0607a1ca28c967caea920b0d19fe39
Kevin Sanchez <kevinadonis0123@gmail.com>
2023-06-28 19:32:15 -0400

    Primer Commit del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header from './components/Header'
+import Proyectos from './components/Proyectos'
+import Tecnologias from './components/Tecnologias'
+import SobreMi from './components/SobreMi'
+
+function App() {
+
+  return (
+    <div className=''>
+      <Header />
+      <Proyectos />
+      <Tecnologias />
+      <SobreMi />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Tecnologias.jsx
@@ -0,0 +1,42 @@
+const Tecnologias = () => {
+  return (
+    <div id="tecnologias" className="bg-sky-500 pb-16 md:pb-0 md:h-screen">
+        <h1 className="text-slate-100 font-bold text-center text-5xl pt-16">Tecnologias</h1>
+
+        <div className="md:grid max-w-[70rem] text-gray-700 md:grid-cols-2 w-[80%] rounded-lg mx-auto mt-16 text-center h-3/5 md:h-1/2 bg-slate-100 gap-4 p-4">
+            <article className="md:mt-10">
+                <h2 className="text-2xl font-bold mb-4">
+                    Front<span className="text-indigo-600">-End</span>
+                </h2>
+                <ul className="leading-10 font-semibold">
+                    <li>Responsive Design</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>TailWindCss</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+
+                </ul>
+            </article>
+
+            <article className="mt-10">
+            <h2 className="text-2xl text-gray-700 font-bold mb-4">
+                    Back<span className="text-indigo-600">-End</span>
+                </h2>
+
+                <ul className="leading-10 font-semibold">
+                    <li>C#</li>
+                    <li>.NET</li>
+                    <li>ASP.NET</li>
+                    <li>Entity Framework Core</li>
+                    <li>PHP</li>
+
+                </ul>
+            </article>
+        </div>
+     
+    </div>
+  )
+}
+
+export default Tecnologias
